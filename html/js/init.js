7975d49c0fef5cbbca5c3c66ca0e5b2c9bc9bdb8
Aaron Crawfis <aacrawfi@microsoft.com>
2019-12-16 20:31:55 -0800

    Updates

--- html/js/init.js
+++ html/js/init.js
@@ -514,7 +514,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Technical Assistant", "Program Manager", "Seattleite" ,"Aspiring Amateur Chef", "Explorer"],
+			strings: ["Technical Assistant", "Program Manager", "Seattleite" ,"Traveler", "Hacker"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
