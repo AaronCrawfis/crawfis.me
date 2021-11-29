fe0b75a4d94efd6c7f83d21ef734f4de3a6f9a38
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2021-11-29 07:25:03 -0800

    Update site with latest info

--- home/js/init.js
+++ home/js/init.js
@@ -514,7 +514,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Technical Assistant", "Program Manager", "Seattleite" ,"Traveler", "Hacker"],
+			strings: ["Program Manager", "Engineer", "Seattleite" ,"Tinkerer", "Maintainer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
